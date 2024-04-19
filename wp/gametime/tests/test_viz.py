caa01255b1ca84b31659a29a3ad9d5dd5338e077
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2024-04-19 15:07:04 +0200

    add specific plotting function from start date

--- wp/gametime/tests/test_viz.py
+++ wp/gametime/tests/test_viz.py
@@ -1,10 +1,11 @@
 import pandas as pd
+import pytest
 import seaborn as sns
 from matplotlib import pyplot as plt
 
 from wp.gametime._config import DF_DTYPES
 from wp.gametime.selection import prepare_dataframe
-from wp.gametime.viz import make_plot_prettier
+from wp.gametime.viz import make_plot_prettier, plot_gametime_barplot
 
 
 def test_make_plot_prettier(gametime_dataframe_fname):
@@ -44,3 +45,63 @@ def test_make_plot_prettier(gametime_dataframe_fname):
     assert isinstance(ax, plt.Axes)
     make_plot_prettier(ax)
     assert isinstance(ax, plt.Axes)
+
+
+def test_plot_gametime_barplot(gametime_dataframe_fname):
+    """Test plotting barplot since inclusion."""
+    df = pd.read_csv(
+        gametime_dataframe_fname, index_col=0, dtype=DF_DTYPES, parse_dates=["acq_time"]
+    )
+    f = plot_gametime_barplot(
+        df,
+        ["76561198329580271", "76561198329580273"],
+        {
+            "76561198329580271": "2024-04-12",
+            "76561198329580273": "2024-04-12",
+        },
+    )
+    assert isinstance(f, plt.Figure)
+
+    df = pd.read_csv(
+        gametime_dataframe_fname, index_col=0, dtype=DF_DTYPES, parse_dates=["acq_time"]
+    )
+    with pytest.warns(RuntimeWarning, match="not at midnight"):
+        f = plot_gametime_barplot(
+            df,
+            ["76561198329580271", "76561198329580273"],
+            {
+                "76561198329580271": "2024-04-12 11:40:00",
+                "76561198329580273": "2024-04-12 13:40:00",
+            },
+        )
+    assert isinstance(f, plt.Figure)
+
+    df = pd.read_csv(
+        gametime_dataframe_fname, index_col=0, dtype=DF_DTYPES, parse_dates=["acq_time"]
+    )
+    df = prepare_dataframe(df, dict())
+    with pytest.warns(RuntimeWarning, match="not at midnight"):
+        f = plot_gametime_barplot(
+            df,
+            ["76561198329580271", "76561198329580273"],
+            {
+                "76561198329580271": "2024-04-12 11:40:00",
+                "76561198329580273": "2024-04-12 13:40:00",
+            },
+        )
+    assert isinstance(f, plt.Figure)
+
+    df = pd.read_csv(
+        gametime_dataframe_fname, index_col=0, dtype=DF_DTYPES, parse_dates=["acq_time"]
+    )
+    df = prepare_dataframe(df, {"76561198329580271": "necromancia"})
+    with pytest.warns(RuntimeWarning, match="not at midnight"):
+        f = plot_gametime_barplot(
+            df,
+            ["necromancia", "76561198329580273"],
+            {
+                "necromancia": "2024-04-12 11:40:00",
+                "76561198329580273": "2024-04-12 13:40:00",
+            },
+        )
+    assert isinstance(f, plt.Figure)
--- wp/gametime/viz.py
+++ wp/gametime/viz.py
@@ -2,10 +2,14 @@ from datetime import datetime
 
 import matplotlib.ticker as mticker
 import numpy as np
+import pandas as pd
 import seaborn as sns
 from matplotlib import pyplot as plt
 
-from ..utils._checks import check_type
+from ..utils._checks import check_gametime_dataframe, check_type, check_value
+from ..utils._docs import fill_doc
+from ..utils.logs import warn
+from .selection import select_datetimes, select_steam_ids
 
 _LABELS: dict[str, str] = {
     "acq_time": "Date/Time (UTC)",
@@ -76,3 +80,72 @@ def _make_ax_prettier(ax: plt.Axes):
     if "steam_id =" in title:
         title = title.split("steam_id =")[1].strip()
         ax.set_title(title)
+
+
+@fill_doc
+def plot_gametime_barplot(
+    df: pd.DataFrame,
+    steam_ids: list[str] | tuple[str, ...],
+    start_dates: dict[str, str | pd.Timestamp],
+) -> plt.Figure:
+    """Plot the total gametime in function of time, per day and per steam ID.
+
+    Parameters
+    ----------
+    %(df_gametime)s
+    steam_ids : list of str | tuple of str
+        List of steam IDs to select.
+    start_dates : dict
+        Mapping of steam IDs to the date (UTC) at which they start the play-phase.
+        The date is either provided as a pd.Timestamp or as a string in the format:
+        'YYYY-MM-DD'.
+
+    Returns
+    -------
+    fig : Figure
+        Matplotlib figure.
+
+    Notes
+    -----
+    If :func:`~wp.gametime.prepare_dataframe` is used, the steam IDs in the selection
+    must match the usernames provided in the mapping.
+    """
+    check_gametime_dataframe(df)
+    check_type(steam_ids, (list, tuple), "steam_ids")
+    for steam_id in steam_ids:
+        check_type(steam_id, (str,), "steam_id")
+    check_type(start_dates, (dict,), "start_dates")
+    for key, value in start_dates.items():
+        check_type(key, (str,), "start_dates key")
+        check_value(key, steam_ids, "start_dates key")
+        check_type(value, (str, pd.Timestamp), "start_dates value")
+    # convert to timestamps, confirm that hours, minute and second are set to 0, else
+    # warn and round
+    for key, value in start_dates.items():
+        if isinstance(value, str):
+            value = pd.Timestamp(value, tz="utc")
+        if value.hour != 0 or value.minute != 0 or value.second != 0:
+            warn(
+                f"Start date for {key} is not at midnight 00:00:00. Rounding to the "
+                "nearest day."
+            )
+        start_dates[key] = value.round("1D")
+    # select steam IDs, then let's create one dataframe per ID with the correct dates
+    df = select_steam_ids(df, steam_ids)
+    dfs = dict()
+    for steam_id in steam_ids:
+        df_ = select_steam_ids(df.copy(deep=True), [steam_id])
+        df_ = select_datetimes(df_, start_dates[steam_id], end=None, freq="1D")
+        df_["Day"] = [str(k + 1) for k in df_.index]
+        dfs[steam_id] = df_
+    # concatenate back the dataframes into a single object and plot
+    df = pd.concat(dfs.values(), ignore_index=True)
+    ax = sns.barplot(
+        df,
+        x="Day",
+        y="game_time",
+        hue="steam_id",
+        errorbar=None,
+    )
+    make_plot_prettier(ax)
+    return ax.figure
